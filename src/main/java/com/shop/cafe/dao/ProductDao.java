06826c300d04ed77e6b5e2463ce4d565f58badf6
KimJunSeo <rlawnstj901+2@gmail.com>
2025-03-05 11:39:22 +0900

    두번쨰수정

--- src/main/java/com/shop/cafe/dao/ProductDao.java
+++ src/main/java/com/shop/cafe/dao/ProductDao.java
@@ -3,6 +3,7 @@ package com.shop.cafe.dao;
 import java.sql.*;
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.shop.cafe.dto.Product;
@@ -10,31 +11,44 @@ import com.shop.cafe.dto.Product;
 @Component
 public class ProductDao {
 	
+	@Value("${spring.datasource.driver-class-name}")
+	private String DB_DRIVER;
+	
+	@Value("${spring.datasource.url}")
+	private String DB_URL;
+	
+	@Value("${spring.datasource.username}")
+	private String DB_USER;
+	
+	@Value("${spring.datasource.password}")
+	private String DB_PW;
+	
+	
+	
 	public List<Product> getAllProducts() throws Exception{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url="jdbc:mysql://localhost:3306/ureca?serverTimezone=UTC";
-		String user="ureca";
-		String pw="ureca";
 		
-		String sql = "Select * from product";
+		Class.forName(DB_DRIVER);		
+		
+		String sql="select * from product";
 		
 		try(
-		Connection con=DriverManager.getConnection(url,user,pw);
-		PreparedStatement stmt = con.prepareStatement(sql);
-		ResultSet rs = stmt.executeQuery();
+		Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PW);
+		PreparedStatement stmt=con.prepareStatement(sql);
+		ResultSet rs=stmt.executeQuery();
 				){
 			
 			List<Product> list=new ArrayList();
 			while(rs.next()) {
-				int prodcode = rs.getInt("prodcode");
+				int prodcode=rs.getInt("prodcode");
 				String prodname=rs.getString("prodname");
-				String pimg = rs.getString("pimg");
-				int price = rs.getInt("price");
+				String pimg=rs.getString("pimg");
+				int price=rs.getInt("price");
 				list.add(new Product(prodcode, price, prodname, pimg));
 			}
 			
 			return list;
+			
 		}
 	}
-	
+
 }
