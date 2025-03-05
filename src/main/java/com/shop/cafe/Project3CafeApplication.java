d8dd5c01d85074961893c748cd100a815126aca1
KimJunSeo <rlawnstj901+2@gmail.com>
2025-03-05 11:30:56 +0900

    첫 구조

---
+++ src/main/java/com/shop/cafe/Project3CafeApplication.java
@@ -0,0 +1,13 @@
+package com.shop.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3CafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3CafeApplication.class, args);
+	}
+
+}
